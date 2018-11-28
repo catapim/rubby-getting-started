d45dc38e12da8409de5c2eb63bec269d577f86c6
Leonardo Luarte González <leonardo@luarte.net>
2018-11-28 18:52:26 -0300

    Cleanup

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,13 +1,6 @@
 class Operator < ApplicationRecord
   has_many :widget_transactions, as: :incumbent
   def ranking
-    tx_count = widget_transactions.count
-    position = WidgetTransaction.where(incumbent_type: self.class.name)
-                                .group(:incumbent_id)
-                                .count
-                                .values
-                                .sort
-                                .reverse
-                                .find_index(tx_count)
+
   end
 end
